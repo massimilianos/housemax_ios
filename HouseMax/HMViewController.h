d2cf488220b720acbf34d360cdf7dc79f4e5f731
Massimiliano <admin@Mac-di-admin.local>
2014-11-06 11:59:41 +0100

    Inizio creazione interfaccia

--- HouseMax/HMViewController.h
+++ HouseMax/HMViewController.h
@@ -10,16 +10,26 @@
 
 @interface HMViewController : UIViewController
 
-@property (weak, nonatomic) IBOutlet UIButton *btnInvia;
-@property (weak, nonatomic) IBOutlet UILabel *lblMessaggio;
-@property (weak, nonatomic) IBOutlet UITextField *txtNome;
-@property (weak, nonatomic) IBOutlet UISwitch *swcInserimentoNome;
-@property (weak, nonatomic) IBOutlet UIButton *btnRichiedi;
+@property (weak, nonatomic) IBOutlet UILabel *lblHouseMax;
+
 @property (weak, nonatomic) IBOutlet UILabel *lblValTemperatura;
 @property (weak, nonatomic) IBOutlet UILabel *lblValUmidita;
+@property (weak, nonatomic) IBOutlet UIButton *btnUpdateTempHum;
 
-- (IBAction)clickInvia:(id)sender;
-- (IBAction)swcChangeInserimentoNome:(id)sender;
-- (IBAction)clickRichiedi:(id)sender;
+@property (weak, nonatomic) IBOutlet UILabel *lblManualEnabler;
+@property (weak, nonatomic) IBOutlet UISwitch *swcManualEnabler;
+
+@property (weak, nonatomic) IBOutlet UILabel *lblRelay1;
+@property (weak, nonatomic) IBOutlet UILabel *lblRelay2;
+@property (weak, nonatomic) IBOutlet UILabel *lblRelay3;
+@property (weak, nonatomic) IBOutlet UILabel *lblRelay4;
+
+@property (weak, nonatomic) IBOutlet UISwitch *swcRelay1;
+@property (weak, nonatomic) IBOutlet UISwitch *swcRelay2;
+@property (weak, nonatomic) IBOutlet UISwitch *swcRelay3;
+@property (weak, nonatomic) IBOutlet UISwitch *swcRelay4;
+
+- (IBAction)clickBtnUpdateTempHum:(id)sender;
+- (IBAction)changeSwcManualEnabler:(id)sender;
 
 @end
--- HouseMax/HMViewController.m
+++ HouseMax/HMViewController.m
@@ -24,50 +24,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)clickInvia:(id)sender {
-    UIAlertView *alertBtnInvia = [[UIAlertView alloc] initWithTitle:@"Bottone INVIA" message:@"Hai premuto il tasto INVIA!" delegate:nil cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
-    
-    [alertBtnInvia show];
-    
-    self.lblMessaggio.text = self.txtNome.text;
-}
-
-- (IBAction)swcChangeInserimentoNome:(id)sender {
-    if (self.swcInserimentoNome.on) {
-        self.txtNome.enabled = TRUE;
-        self.btnInvia.enabled = TRUE;
-    } else {
-        self.txtNome.enabled = FALSE;
-        self.btnInvia.enabled = FALSE;
-    }
-}
-
-- (IBAction)clickRichiedi:(id)sender {
-//  Aggiorno i valori di temperatura ed umidita'
-    NSURL *urlUpdateTemperaturaUmidita = [NSURL URLWithString:@"http://massimilianos.ns0.it:82/tempRead/0/0"];
-    [NSData dataWithContentsOfURL:urlUpdateTemperaturaUmidita];
-    
-//  Leggo il valore della temperatura
-    NSURLRequest *urlVisTemperatura=[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://massimilianos.ns0.it/domusalberti/dati/ElaboraDati.php?operazione=select&valore=temperatura"]
-                                                  cachePolicy:NSURLRequestUseProtocolCachePolicy
-                                                  timeoutInterval:60.0];
-    NSURLResponse *responseVisTemperatura = nil;
-    NSData *dataVisTemperatura = [NSURLConnection sendSynchronousRequest:urlVisTemperatura returningResponse:&responseVisTemperatura error:nil];
-    NSString *resVisTemperatura = [[NSString alloc] initWithData:dataVisTemperatura encoding:NSUTF8StringEncoding];
-
-    self.lblValTemperatura.text = [NSString stringWithFormat:@"\"%@°\"", resVisTemperatura];
-
-//  Leggo il valore dell'umidita'
-    NSURLRequest *urlVisUmidita=[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://massimilianos.ns0.it/domusalberti/dati/ElaboraDati.php?operazione=select&valore=umidita"]
-                                              cachePolicy:NSURLRequestUseProtocolCachePolicy
-                                              timeoutInterval:60.0];
-    NSURLResponse *responseVisUmidita = nil;
-    NSData *dataVisUmidita = [NSURLConnection sendSynchronousRequest:urlVisUmidita returningResponse:&responseVisUmidita error:nil];
-    NSString *resVisUmidita = [[NSString alloc] initWithData:dataVisUmidita encoding:NSUTF8StringEncoding];
-    
-    self.lblValUmidita.text = [NSString stringWithFormat:@"\"%@%%\"", resVisUmidita];
-}
-
 /*
  Questo metodo serve per far sparire la tastiera una volta finito
  usarla, altrimenti rimarrebbe sullo schermo
@@ -76,4 +32,52 @@
     [self.view endEditing:YES];
 }
 
+- (IBAction)clickBtnUpdateTempHum:(id)sender {
+    //    UIAlertView *alertBtnInvia = [[UIAlertView alloc] initWithTitle:@"Bottone INVIA" message:@"Hai premuto il tasto INVIA!" delegate:nil cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
+    //    [alertBtnInvia show];
+    
+    //  Aggiorno i valori di temperatura ed umidita'
+    NSURL *urlUpdateTemperaturaUmidita = [NSURL URLWithString:@"http://massimilianos.ns0.it:82/tempRead/0/0"];
+    [NSData dataWithContentsOfURL:urlUpdateTemperaturaUmidita];
+    
+    //  Leggo il valore della temperatura
+    NSURLRequest *urlVisTemperatura=[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://massimilianos.ns0.it/domusalberti/dati/ElaboraDati.php?operazione=select&valore=temperatura"] cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0];
+    NSURLResponse *responseVisTemperatura = nil;
+    NSData *dataVisTemperatura = [NSURLConnection sendSynchronousRequest:urlVisTemperatura returningResponse:&responseVisTemperatura error:nil];
+    NSString *resVisTemperatura = [[NSString alloc] initWithData:dataVisTemperatura encoding:NSUTF8StringEncoding];
+    
+    self.lblValTemperatura.text = [NSString stringWithFormat:@"\"%@°\"", resVisTemperatura];
+    
+    //  Leggo il valore dell'umidita'
+    NSURLRequest *urlVisUmidita=[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://massimilianos.ns0.it/domusalberti/dati/ElaboraDati.php?operazione=select&valore=umidita"] cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0];
+    NSURLResponse *responseVisUmidita = nil;
+    NSData *dataVisUmidita = [NSURLConnection sendSynchronousRequest:urlVisUmidita returningResponse:&responseVisUmidita error:nil];
+    NSString *resVisUmidita = [[NSString alloc] initWithData:dataVisUmidita encoding:NSUTF8StringEncoding];
+    
+    self.lblValUmidita.text = [NSString stringWithFormat:@"\"%@%%\"", resVisUmidita];
+}
+
+- (IBAction)changeSwcManualEnabler:(id)sender {
+    if (self.swcManualEnabler.on) {
+        self.lblRelay1.enabled = TRUE;
+        self.lblRelay2.enabled = TRUE;
+        self.lblRelay3.enabled = TRUE;
+        self.lblRelay4.enabled = TRUE;
+        
+        self.swcRelay1.enabled = TRUE;
+        self.swcRelay2.enabled = TRUE;
+        self.swcRelay3.enabled = TRUE;
+        self.swcRelay4.enabled = TRUE;
+    } else {
+        self.lblRelay1.enabled = FALSE;
+        self.lblRelay2.enabled = FALSE;
+        self.lblRelay3.enabled = FALSE;
+        self.lblRelay4.enabled = FALSE;
+        
+        self.swcRelay1.enabled = FALSE;
+        self.swcRelay2.enabled = FALSE;
+        self.swcRelay3.enabled = FALSE;
+        self.swcRelay4.enabled = FALSE;
+    }
+}
 @end
